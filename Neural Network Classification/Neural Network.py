c8fc185f32f04afbb918450fcc92ef7540d9a6b0
PauloCapHeimdall <pasantos.heimdall@lidercap.com.br>
2026-06-17 17:00:36 -0300

    Finaliazacao Iniciacao NN

--- Neural Network Classification/Neural Network.py
+++ Neural Network Classification/Neural Network.py
@@ -12,3 +12,23 @@ class NeuralNetwork:
 
         #pesos de cada camada
         self.weights = []
+        
+        #bias das camadas
+        self.bias = []
+
+        for i in range(self.num_layer - 1):
+            n_in = layer_sizes[i]
+            n_out = layer_sizes[i+1]
+
+            limit = np.sqrt(2.0/n_in)
+            w = self.rng.normal(loc=0.0, scale = limit, size=(n_in,n_out))
+            b = np.zeros((1,n_out))
+
+            self.weights.append(w)
+            self.bias.append(b)
+
+    def __repr__(self):
+        arch = " -> ".join(str(n) for n in self.layer_sizes)
+        return f"NeuralNet({arch})"
+    
+
