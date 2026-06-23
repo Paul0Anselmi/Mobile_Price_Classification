b6d04a90fa9b8d0a3c1d775b426c22d77d9462ab
PauloCapHeimdall <pasantos.heimdall@lidercap.com.br>
2026-06-22 23:30:58 -0300

    Forward

--- Neural Network Classification/Neural Network.py
+++ Neural Network Classification/Neural Network.py
@@ -32,4 +32,42 @@ class NeuralNetwork:
         arch = " -> ".join(str(n) for n in self.layer_sizes)
         return f"NeuralNet({arch})"
     
-
+    def forward(self, X):
+            """
+            Propaga as entradas X pela rede (forward pass).
+    
+            Parameters
+            ----------
+            X : np.ndarray, shape (n_amostras, n_features)
+                Dados de entrada. Cada linha e uma amostra (um celular),
+                cada coluna e uma das 20 variaveis do dataset.
+    
+            Returns
+            -------
+            activations : list[np.ndarray]
+                Ativacoes de cada camada, incluindo a entrada (camada 0)
+                e a saida (ultima camada). Guardadas para uso no backprop.
+            pre_activations : list[np.ndarray]
+                Valores lineares (z = X @ W + b) antes de aplicar a funcao
+                de ativacao. Tambem necessarios no backprop.
+            """
+            activations = [X]       # a[0] = entrada bruta
+            pre_activations = []    # z[i] = combinacao linear da camada i
+    
+            a = X
+            for i in range(self.num_layers - 1):
+                # Passo 1: combinacao linear z = a @ W + b
+                z = a @ self.weights[i] + self.bias[i]
+                pre_activations.append(z)
+    
+                # Passo 2: aplicar ativacao
+                # Camadas ocultas -> ReLU
+                # Camada de saida (ultima) -> softmax
+                if i < self.num_layers - 2:
+                    a = relu(z)
+                else:
+                    a = softmax(z)
+    
+                activations.append(a)
+    
+            return activations, pre_activations
